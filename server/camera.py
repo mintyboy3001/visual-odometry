f85478d32f8568a04c0722d63d871d4d812f302f
Marius-Catalin Popa <sc19mcp@leeds.ac.uk>
2024-01-20 17:05:55 +0000

    structure

---
+++ server/camera.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2 as cv
+
+stream_url = 'tcp://192.168.0.31:8123'
+
+cap = cv.VideoCapture(stream_url)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
