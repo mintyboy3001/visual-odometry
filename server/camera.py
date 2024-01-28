54451adf855e66b818f83088cbb5af898232ab26
mintyboi3001 <128733994+mintyboy3001@users.noreply.github.com>
2024-01-28 19:09:53 +0000

    refactor and new functionality took some code from https://github.com/niconielsen32/ComputerVision/blob/master/visual_odometry.py trying to fit it to my own purpose this seems to be the standard way to compute visual odometry
    todo: camera calibration

--- server/camera.py
+++ server/camera.py
@@ -1,25 +1,191 @@
 import numpy as np
 import cv2 as cv
 
-stream_url = 'tcp://192.168.0.31:8123'
+import time
 
-cap = cv.VideoCapture(stream_url)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+
+
+class CameraOdometry:
+
+    def __init__(self,stream_url='tcp://192.168.0.31:8123'):
+        self.cap = cv.VideoCapture(stream_url)
+        if not self.cap.isOpened():
+            print("Cannot open camera")
+            exit()
+        self.orb = cv.ORB.create()
+
+        FLANN_INDEX_LSH = 6
+        index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6, key_size=12, multi_probe_level=1)
+        search_params = dict(checks=50)
+        self.flann = cv.FlannBasedMatcher(indexParams=index_params, searchParams=search_params)
+
+
+    def getKp(self,frame):
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        return self.orb.detectAndCompute(gray,None)
+    
+
+    def get_matches(self, prev,curr):
+        """
+        This function detect and compute keypoints and descriptors from the i-1'th and i'th image using the class orb object
+
+        Parameters
+        ----------
+        i (int): The current frame
+
+        Returns
+        -------
+        q1 (ndarray): The good keypoints matches position in i-1'th image
+        q2 (ndarray): The good keypoints matches position in i'th image
+        """
+        # Find the keypoints and descriptors with ORB
+        kp1, des1 = self.getKp(prev)
+        kp2, des2 = self.getKp(curr)
+        # Find matches
+        matches = self.flann.knnMatch(des1, des2, k=2)
+
+        # Find the matches there do not have a to high distance
+        good = []
+        try:
+            for m, n in matches:
+                if m.distance < 0.8 * n.distance:
+                    good.append(m)
+        except ValueError:
+            pass
+
+        draw_params = dict(matchColor = -1, # draw matches in green color
+                 singlePointColor = None,
+                 matchesMask = None, # draw only inliers
+                 flags = 2)
+
+        img3 = cv.drawMatches(prev, kp1, curr,kp2, good ,None,**draw_params)
+        cv.imshow("image", img3)
+        cv.waitKey(200)
+
+        # Get the image points form the good matches
+        q1 = np.float32([kp1[m.queryIdx].pt for m in good])
+        q2 = np.float32([kp2[m.trainIdx].pt for m in good])
+        return q1, q2
+    
+    def get_pose(self, q1, q2):
+        """
+        Calculates the transformation matrix
+
+        Parameters
+        ----------
+        q1 (ndarray): The good keypoints matches position in i-1'th image
+        q2 (ndarray): The good keypoints matches position in i'th image
+
+        Returns
+        -------
+        transformation_matrix (ndarray): The transformation matrix
+        """
+        # Essential matrix
+        E, _ = cv.findEssentialMat(q1, q2, self.K, threshold=1)
+
+        # Decompose the Essential matrix into R and t
+        R, t = self.decomp_essential_mat(E, q1, q2)
+
+        # Get transformation matrix
+        transformation_matrix = self._form_transf(R, np.squeeze(t))
+        return transformation_matrix
+    
+    def decomp_essential_mat(self, E, q1, q2):
+        """
+        Decompose the Essential matrix
+
+        Parameters
+        ----------
+        E (ndarray): Essential matrix
+        q1 (ndarray): The good keypoints matches position in i-1'th image
+        q2 (ndarray): The good keypoints matches position in i'th image
+
+        Returns
+        -------
+        right_pair (list): Contains the rotation matrix and translation vector
+        """
+        def sum_z_cal_relative_scale(R, t):
+            # Get the transformation matrix
+            T = self._form_transf(R, t)
+            # Make the projection matrix
+            P = np.matmul(np.concatenate((self.K, np.zeros((3, 1))), axis=1), T)
+
+            # Triangulate the 3D points
+            hom_Q1 = cv.triangulatePoints(self.P, P, q1.T, q2.T)
+            # Also seen from cam 2
+            hom_Q2 = np.matmul(T, hom_Q1)
+
+            # Un-homogenize
+            uhom_Q1 = hom_Q1[:3, :] / hom_Q1[3, :]
+            uhom_Q2 = hom_Q2[:3, :] / hom_Q2[3, :]
+
+            # Find the number of points there has positive z coordinate in both cameras
+            sum_of_pos_z_Q1 = sum(uhom_Q1[2, :] > 0)
+            sum_of_pos_z_Q2 = sum(uhom_Q2[2, :] > 0)
+
+            # Form point pairs and calculate the relative scale
+            relative_scale = np.mean(np.linalg.norm(uhom_Q1.T[:-1] - uhom_Q1.T[1:], axis=-1)/
+                                     np.linalg.norm(uhom_Q2.T[:-1] - uhom_Q2.T[1:], axis=-1))
+            return sum_of_pos_z_Q1 + sum_of_pos_z_Q2, relative_scale
+
+        # Decompose the essential matrix
+        R1, R2, t = cv.decomposeEssentialMat(E)
+        t = np.squeeze(t)
+
+        # Make a list of the different possible pairs
+        pairs = [[R1, t], [R1, -t], [R2, t], [R2, -t]]
+
+        # Check which solution there is the right one
+        z_sums = []
+        relative_scales = []
+        for R, t in pairs:
+            z_sum, scale = sum_z_cal_relative_scale(R, t)
+            z_sums.append(z_sum)
+            relative_scales.append(scale)
+
+        # Select the pair there has the most points with positive z coordinate
+        right_pair_idx = np.argmax(z_sums)
+        right_pair = pairs[right_pair_idx]
+        relative_scale = relative_scales[right_pair_idx]
+        R1, t = right_pair
+        t = t * relative_scale
+
+        return [R1, t]
+
+
+    def spin(self):
+        t0 = time.time()
+        ret, frame = self.cap.read()
+        prev = frame
+        while True:
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            # if frame is read correctly ret is Truema
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+        
+            #live cap
+            cv.imshow('frame', frame)
+            
+            t = time.time()
+
+            #cap it to every second
+            if t - t0 >= 1:
+                curr = frame
+                self.get_matches(prev,curr)
+                prev = curr
+                #take computation time into account
+                t0 = time.time()
+
+            if cv.waitKey(1) == ord('q'):
+                break
+        # When everything done, release the capture
+        self.cap.release()
+        cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    cam = CameraOdometry()
+    cam.spin()
